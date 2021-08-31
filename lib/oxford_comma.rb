b8a52b6442228153caf2254c704d70c4588a7205
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-08-31 12:10:19 -0700

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ require 'pry'
 def oxford_comma(array)
     #returns a string with any additional formatting when given 1-element
     if array.size == 1 
-        array.join("")
+        array[0]
     elsif array.size == 2 
         array.join(" and ")
-    elsif array.size >= 3 
+    elsif array.size >= 2 
         array[-1].insert(0, "and ") #takes the last element in an array and inserts and add within that last element
         array.join(", ")
     end
