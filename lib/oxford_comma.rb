7cad33b829b982aa5f080379fca75a2f28dd5180
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-08-31 11:57:50 -0700

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def oxford_comma(array)
-
+    #returns a string with any additional formatting when given 1-element
+    if array.size == 1 
+        array.join("")
+    elsif array.size == 2 
+        array.join(" and ")
+    elsif array.size >= 3 
+        last_ele = array.pop
+        new_ele = "and " + last_ele
+        array.push(new_ele).join(", ")
+    end
 end
+
+
